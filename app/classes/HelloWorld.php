6746fe45d19c1254901013f2275e4cf556137b2f
robincse444 <hossaincse444@gmail.com>
2022-08-02 12:49:04 +0600

    nested loop

--- app/classes/HelloWorld.php
+++ app/classes/HelloWorld.php
@@ -9,6 +9,7 @@ class HelloWorld
     public $y;
     public $z;
     public $check;
+    public $data = [];
 
     public function __construct()
     {
@@ -21,33 +22,117 @@ class HelloWorld
 
     public function index()
     {
-        $this->x = 30;
-        $this->y = 200;
-        $this->z =$this->x + $this->y;
+        $this->data = [
+            0 => [
+                'name'      => 'Polash',
+                'email'     => 'plolash@gmail.com',
+                'mobile'    => [
 
-        do
-        {
-            echo $this->x;
-            $this->x++;
-        }while($this->x > 50);
+                     'person'   => '1111',
+                     'gurdian'  => '2564',
+                ]
+            ],
 
-        while ($this->x < 50)
-        {
-            echo $this->x.' ';
-            $this->x++;
-        }
+            1 => [
+                'name'      => 'karim',
+                'email'     => 'karim@gmail.com',
+                'mobile'    => [
 
-        for ($this->x; $this->x <= 100; $this->x++)
+                    'person'   => '3333',
+                    'gurdian'  => '8954',
+                ]
+            ],
+
+            2 => [
+                'name'      => 'sahin',
+                'email'     => 'karim@gmail.com',
+                'mobile'    => [
+
+                    'person' => '4444',
+                    'gurdian' => '3256',
+                ]
+            ],
+
+         ];
+
+        foreach ($this->data as $item)
         {
-            if ($this->x == 15)
+//            echo $item['name'].$item['email'].$item['mobile'].'<br/>';
+            foreach ($item as $value)
             {
-                break;
+                if (is_array($value))
+                {
+                    foreach ($value as $v_item)
+                    {
+                        echo $v_item;
+                    }
+                }
+                else
+                {
+                    echo $value.' ';
+                }
             }
-            else{
-                continue;
-            }
-            echo $this->x.'<br/>';
+            echo '<br/>';
         }
+//        echo $this->data[1]['name'];
+
+
+
+
+
+
+//        foreach ($this->data as $index => $item)
+//        {
+//            echo "index : ".$index. "value : ".$item.' ';
+//        }
+//        echo $this->data['name'];
+
+
+//        $this->data = [100, 200, 300, "BITM", "SEIP", true, false, 50.20];
+//
+//         echo $this->data[1];
+//
+//        foreach ($this->data as $item)
+//        {
+//            echo $item.' ';
+//        }
+
+
+
+
+
+
+
+
+
+
+//        $this->x = 30;
+//        $this->y = 200;
+//        $this->z =$this->x + $this->y;
+//
+//        do
+//        {
+//            echo $this->x;
+//            $this->x++;
+//        }while($this->x > 50);
+//
+//        while ($this->x < 50)
+//        {
+//            echo $this->x.' ';
+//            $this->x++;
+//        }
+//
+//        for ($this->x; $this->x <= 100; $this->x++)
+//        {
+//            if ($this->x == 15)
+//            {
+//                break;
+//            }
+//            else{
+//                continue;
+//            }
+//            echo $this->x.'<br/>';
+//        }
 
 //        switch ($this->x)
 //        {
